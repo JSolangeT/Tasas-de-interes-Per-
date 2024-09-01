bad3798d194fd378433548e8fa6352448990f4bc
Joselyn Tejeda <Jstc1001@gmail.com>
2024-09-01 00:55:02 -0500

    nuevo codigo del codigo nuevo de py

--- codigo.py
+++ codigo.py
@@ -1,8 +1,12 @@
+#lametablemente no tengo mucho tiempo para alterar mucho el codigo jeje asi que solo agregaremos este comentario totalmente importante
 
-a= print(input("Ingresa tu palabra"))
+
+a= print(input("Ingresa un número"))
+b = print(input("Ingresa otro número"))
+c = print(input("Ingresa otro número"))
 
 
-if a == "numero" :
+if a < 10 and b > 12 and c == 15 :
     for i in range (1,6):
         print ("Este es el número", i)
 else: 
