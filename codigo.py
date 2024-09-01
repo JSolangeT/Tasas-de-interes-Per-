ee493b0ca5ba97362a299470ca91d388e25bd3db
Joselyn Tejeda <Jstc1001@gmail.com>
2024-09-01 00:23:40 -0500

    Vamos a agregar la modificación del codigo en Py

--- codigo.py
+++ codigo.py
@@ -1,2 +1,9 @@
-for i in range (1,6):
-    print ("Este es el número", i)
+
+a= print(input("Ingresa tu palabra"))
+
+
+if a == "numero" :
+    for i in range (1,6):
+        print ("Este es el número", i)
+else: 
+    "No hay ninguna asignación"
