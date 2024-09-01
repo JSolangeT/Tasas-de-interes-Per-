09455657f286d63d25e6cd737754edabb1229940
Joselyn Tejeda <Jstc1001@gmail.com>
2024-09-01 01:01:30 -0500

    vamos a funcionar

--- codigo.py
+++ codigo.py
@@ -1,12 +1,13 @@
-a= print(input("Ingresa tu palabra 1"))
-b = a= print(input("Ingresa tu palabra 2"))
+#lametablemente no tengo mucho tiempo para alterar mucho el codigo jeje asi que solo agregaremos este comentario totalmente importante
 
 
+a= print(input("Ingresa un número"))
+b = print(input("Ingresa otro número"))
+c = print(input("Ingresa otro número"))
+d = print(input("Ingresa otro número"))
 
-if a == "rapido" :
+if a < 25 and b > 12 and c == 18 :
     for i in range (1,6):
         print ("Este es el número", i)
-elif b == "furioso" :
-    print("rapidos y friosos")
 else: 
-    "No hay ninguna asignación"
+    "No se asigno ningun número"
