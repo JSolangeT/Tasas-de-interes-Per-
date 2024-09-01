8d63436fcbf43649d2c328a73919d87c61f0cdd3
Joselyn Tejeda <Jstc1001@gmail.com>
2024-09-01 00:38:28 -0500

    agregamos el codigo de py cambiado

---
+++ codigo.py
@@ -0,0 +1,13 @@
+
+a= print(input("Ingresa tu palabra 1"))
+b = a= print(input("Ingresa tu palabra 2"))
+
+
+
+if a == "rapido" :
+    for i in range (1,6):
+        print ("Este es el número", i)
+elif b == "furioso" :
+    print("rapidos y friosos")
+else: 
+    "No hay ninguna asignación"
